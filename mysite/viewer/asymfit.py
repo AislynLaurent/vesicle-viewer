df1a6362ae470905e548764672056c2813fd3f41
AislynLaurent <40035557+AislynLaurent@users.noreply.github.com>
2020-08-11 14:04:35 -0400

    SDP Fix

--- mysite/viewer/asymfit.py
+++ mysite/viewer/asymfit.py
@@ -715,7 +715,7 @@ def asymmetrical_fit(parameter, in_sample_lipids, out_sample_lipids, datas, temp
 
     return fit_result
 
-def asymmetrical_sdp(parameter, in_head_prob, in_chain_prob, in_tm_prob, in_water_prob, out_head_prob, out_chain_prob, out_tm_prob, out_water_prob, in_sample_lipids, out_sample_lipids, data, temp):
+def asymmetrical_sdp(parameter, in_head_prob, in_methyl_prob, in_tm_prob, in_water_prob, out_head_prob, out_methyl_prob, out_tm_prob, out_water_prob, in_sample_lipids, out_sample_lipids, data, temp):
     # Declare
     in_sdp_final = []
     out_sdp_final = []
@@ -753,23 +753,23 @@ def asymmetrical_sdp(parameter, in_head_prob, in_chain_prob, in_tm_prob, in_wate
     ## Scale probabilities
     # Inner
     in_scaled_head_prob = (np.asarray(in_head_prob)*(bhi/Vhi))
-    in_scaled_chain_prob = (np.asarray(in_chain_prob)*(bci/Vci))
+    in_scaled_methyl_prob = (np.asarray(in_methyl_prob)*(bci/Vci))
     in_scaled_tm_prob = (np.asarray(in_tm_prob)*(bti/Vti))
     # Outter
     out_scaled_head_prob = (np.asarray(out_head_prob)*(bho/Vho))
-    out_scaled_chain_prob = (np.asarray(out_chain_prob)*(bco/Vco))
+    out_scaled_methyl_prob = (np.asarray(out_methyl_prob)*(bco/Vco))
     out_scaled_tm_prob = (np.asarray(out_tm_prob)*(bto/Vto))
     # Water
     in_scaled_water_prob = (np.asarray(in_water_prob)*(bw/Vw))
     out_scaled_water_prob = (np.asarray(out_water_prob)*(bw/Vw))
 
     # Add them together
-    for in_h_value, in_c_value, in_tm_value, in_w_value in zip(in_scaled_head_prob, in_scaled_chain_prob, in_scaled_tm_prob, in_scaled_water_prob):
-        in_sdp_final.append(in_h_value+in_c_value+in_tm_value+in_w_value)
+    for in_h_value, in_m_value, in_tm_value, in_w_value in zip(in_scaled_head_prob, in_scaled_methyl_prob, in_scaled_tm_prob, in_scaled_water_prob):
+        in_sdp_final.append(in_h_value+in_m_value+in_tm_value+in_w_value)
 
-    for out_h_value, out_c_value, out_tm_value, out_w_value in zip(out_scaled_head_prob, out_scaled_chain_prob, out_scaled_tm_prob, out_scaled_water_prob):
-        out_sdp_final.append(out_h_value+out_c_value+out_tm_value+out_w_value)
+    for out_h_value, out_m_value, out_tm_value, out_w_value in zip(out_scaled_head_prob, out_scaled_methyl_prob, out_scaled_tm_prob, out_scaled_water_prob):
+        out_sdp_final.append(out_h_value+out_m_value+out_tm_value+out_w_value)
 
-    combined_sdp = [in_sdp_final, out_sdp_final, in_scaled_head_prob, in_scaled_chain_prob, in_scaled_tm_prob, out_scaled_head_prob, out_scaled_chain_prob, out_scaled_tm_prob, in_scaled_water_prob, out_water_prob]
+    combined_sdp = [in_sdp_final, out_sdp_final, in_scaled_head_prob, in_scaled_methyl_prob, in_scaled_tm_prob, out_scaled_head_prob, out_scaled_methyl_prob, out_scaled_tm_prob, in_scaled_water_prob, out_water_prob]
 
     return(combined_sdp)
