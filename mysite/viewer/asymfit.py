d8c2647e71021dc289f4f4cecfe216040dfff93f
AislynLaurent <40035557+AislynLaurent@users.noreply.github.com>
2020-10-14 12:32:39 -0400

    Extra comments

--- mysite/viewer/asymfit.py
+++ mysite/viewer/asymfit.py
@@ -11,6 +11,7 @@ from .symprobabilities import *
 from scipy import signal as sig
 
 # Asymmetrical model
+# The math only - used by LMFit to run each iteration
 def asym_model(
     q,          # independant
     Vci,        # chain_volume
@@ -91,6 +92,7 @@ def asym_model(
     )
 
 # Asymmetrical model and separated form factor
+# The same math, but including the separated form factor equation
 def asym_model_separated(
     q,          # independant
     Vci,        # chain_volume
@@ -185,6 +187,7 @@ def asym_model_separated(
     )
 
 # Calculate result from model for an individual dataset
+# Connetc the model, the dataset and the parameters
 def calc_asym_model(fit_parameters, q, data, sff):
     # Convert array
     q_array = np.array(q)
@@ -235,7 +238,8 @@ def calc_asym_model(fit_parameters, q, data, sff):
     return calc_result
 
 
-# Objective function create a residual for each, then flatten
+# Objective function
+# Create a residual for each dataset, then flatten into a single array for minimize()
 def asymmetrical_objective_function(fit_parameters, x, datas, sff):
     # Delcare
     current_residual = []
@@ -303,6 +307,7 @@ def asymmetrical_objective_function(fit_parameters, x, datas, sff):
     return combined_residuals
 
 # Augments per data-set
+# Calculate augmentations for each dataset as requried
 def adjust_b_values(data, in_sample_lipids, out_sample_lipids, water, d_water, temp):
     # Query
     augments = Data_Sample_Lipid_Augment.objects.filter(data_set_title=data)
@@ -344,12 +349,15 @@ def adjust_b_values(data, in_sample_lipids, out_sample_lipids, water, d_water, t
         )
     )
 
+    # If it's x-ary data, work with the electron values
     if data.data_type == 'XR':
         # bw
         water_b = water.electrons
 
         ## Inner
+        # Check each lipid in the sample
         for sample_lipid in in_sample_lipids:
+            # If it's a standard lipid, take values from there [b_value = electrons * mol fraction]
             if sample_lipid.sample_lipid_name.project_lipid_name:
                 # bt
                 in_terminal_methyl_b = in_terminal_methyl_b + (sample_lipid.sample_lipid_name.project_lipid_name.tm_electrons * sample_lipid.lipid_mol_fraction)
@@ -357,6 +365,7 @@ def adjust_b_values(data, in_sample_lipids, out_sample_lipids, water, d_water, t
                 in_chain_b = in_chain_b + (sample_lipid.sample_lipid_name.project_lipid_name.tg_electrons * sample_lipid.lipid_mol_fraction)
                 # bh
                 in_headgroup_b = in_headgroup_b + (sample_lipid.sample_lipid_name.project_lipid_name.hg_electrons * sample_lipid.lipid_mol_fraction)
+            # If it's a custom lipid check for user entered values [b_value = electrons * mol fraction]
             else:
                 # bt
                 in_terminal_methyl_b = in_terminal_methyl_b + (sample_lipid.sample_lipid_name.project_user_lipid_name.tm_electrons * sample_lipid.lipid_mol_fraction)
@@ -365,7 +374,9 @@ def adjust_b_values(data, in_sample_lipids, out_sample_lipids, water, d_water, t
                 # bh
                 in_headgroup_b = in_headgroup_b + (sample_lipid.sample_lipid_name.project_user_lipid_name.hg_electrons * sample_lipid.lipid_mol_fraction)
         ## Outer
+        # Check each lipid in the sample
         for sample_lipid in out_sample_lipids:
+            # If it's a standard lipid, take values from there [b_value = electrons * mol fraction]
             if sample_lipid.sample_lipid_name.project_lipid_name:
                 # bt
                 out_terminal_methyl_b = out_terminal_methyl_b + (sample_lipid.sample_lipid_name.project_lipid_name.tm_electrons * sample_lipid.lipid_mol_fraction)
@@ -373,6 +384,7 @@ def adjust_b_values(data, in_sample_lipids, out_sample_lipids, water, d_water, t
                 out_chain_b = out_chain_b + (sample_lipid.sample_lipid_name.project_lipid_name.tg_electrons * sample_lipid.lipid_mol_fraction)
                 # bh
                 out_headgroup_b = out_headgroup_b + (sample_lipid.sample_lipid_name.project_lipid_name.hg_electrons * sample_lipid.lipid_mol_fraction)
+            # If it's a custom lipid check for user entered values [b_value = electrons * mol fraction]
             else:
                 # bt
                 out_terminal_methyl_b = out_terminal_methyl_b + (sample_lipid.sample_lipid_name.project_user_lipid_name.tm_electrons * sample_lipid.lipid_mol_fraction)
@@ -380,14 +392,23 @@ def adjust_b_values(data, in_sample_lipids, out_sample_lipids, water, d_water, t
                 out_chain_b = out_chain_b + (sample_lipid.sample_lipid_name.project_user_lipid_name.tg_electrons * sample_lipid.lipid_mol_fraction)
                 # bh
                 out_headgroup_b = out_headgroup_b + (sample_lipid.sample_lipid_name.project_user_lipid_name.hg_electrons * sample_lipid.lipid_mol_fraction)
+    
+    # If it's neturon data, work with scattering values
     else:
         # bw
         water_b = (d_water.scattering_length * data.d2o_mol_fraction) + (water.scattering_length * (1 - data.d2o_mol_fraction))
 
         # Inner
+        # Check each lipid in the sample
         for sample_lipid in in_sample_lipids:
+            
+            # If it's a standard lipid, take values from there
             if sample_lipid.sample_lipid_name.project_lipid_name:
+                
+                # If there is an augmentation (i.e. the user has indicated it's deuterated), calculate the scaled values [b_value = (scattering length + net change) * mol fraction]
                 if sample_lipid in in_augment_dict.keys():
+                    
+                    # For standard augmentations
                     if in_augment_dict[sample_lipid].sample_lipid_augment:
                         # bt
                         in_terminal_methyl_b = in_terminal_methyl_b + ((sample_lipid.sample_lipid_name.project_lipid_name.tm_scattering + in_augment_dict[sample_lipid].sample_lipid_augment.tmg_scattering_net_change) * sample_lipid.lipid_mol_fraction)
@@ -395,6 +416,8 @@ def adjust_b_values(data, in_sample_lipids, out_sample_lipids, water, d_water, t
                         in_chain_b = in_chain_b + ((sample_lipid.sample_lipid_name.project_lipid_name.tg_scattering + in_augment_dict[sample_lipid].sample_lipid_augment.tg_scattering_net_change) * sample_lipid.lipid_mol_fraction)
                         # bh
                         in_headgroup_b = in_headgroup_b + ((sample_lipid.sample_lipid_name.project_lipid_name.hg_scattering + in_augment_dict[sample_lipid].sample_lipid_augment.hg_scattering_net_change) * sample_lipid.lipid_mol_fraction)
+                    
+                    # For custom augmentations
                     else:
                         # bt
                         in_terminal_methyl_b = in_terminal_methyl_b + ((sample_lipid.sample_lipid_name.project_lipid_name.tm_scattering + in_augment_dict[sample_lipid].sample_lipid_custom_augment.tmg_scattering_net_change) * sample_lipid.lipid_mol_fraction)
@@ -402,6 +425,8 @@ def adjust_b_values(data, in_sample_lipids, out_sample_lipids, water, d_water, t
                         in_chain_b = in_chain_b + ((sample_lipid.sample_lipid_name.project_lipid_name.tg_scattering + in_augment_dict[sample_lipid].sample_lipid_custom_augment.tg_scattering_net_change) * sample_lipid.lipid_mol_fraction)
                         # bh
                         in_headgroup_b = in_headgroup_b + ((sample_lipid.sample_lipid_name.project_lipid_name.hg_scattering + in_augment_dict[sample_lipid].sample_lipid_custom_augment.hg_scattering_net_change) * sample_lipid.lipid_mol_fraction)
+                
+                # If there isn't any augmentation, take the scatting length without scaling it [b_value = scattering length * mol fraction]
                 else:
                     # bt
                     in_terminal_methyl_b = in_terminal_methyl_b + (sample_lipid.sample_lipid_name.project_lipid_name.tm_scattering * sample_lipid.lipid_mol_fraction)
@@ -409,7 +434,11 @@ def adjust_b_values(data, in_sample_lipids, out_sample_lipids, water, d_water, t
                     in_chain_b = in_chain_b + (sample_lipid.sample_lipid_name.project_lipid_name.tg_scattering * sample_lipid.lipid_mol_fraction)
                     # bh
                     in_headgroup_b = in_headgroup_b + (sample_lipid.sample_lipid_name.project_lipid_name.hg_scattering * sample_lipid.lipid_mol_fraction)
+            
+            # If it's a custom lipid check for user entered values
             else:
+                
+                # If there is an augmentation (i.e. the user has indicated it's deuterated), calculate the scaled values [b_value = (scattering length + net change) * mol fraction]
                 if sample_lipid in in_augment_dict.keys():
                     # bt
                     in_terminal_methyl_b = in_terminal_methyl_b + ((sample_lipid.sample_lipid_name.project_user_lipid_name.tm_scattering + in_augment_dict[sample_lipid].sample_lipid_custom_augment.tmg_scattering_net_change) * sample_lipid.lipid_mol_fraction)
@@ -417,6 +446,8 @@ def adjust_b_values(data, in_sample_lipids, out_sample_lipids, water, d_water, t
                     in_chain_b = in_chain_b + ((sample_lipid.sample_lipid_name.project_user_lipid_name.tg_scattering + in_augment_dict[sample_lipid].sample_lipid_custom_augment.tg_scattering_net_change) * sample_lipid.lipid_mol_fraction)
                     # bh
                     in_headgroup_b = in_headgroup_b + ((sample_lipid.sample_lipid_name.project_user_lipid_name.hg_scattering + in_augment_dict[sample_lipid].sample_lipid_custom_augment.hg_scattering_net_change) * sample_lipid.lipid_mol_fraction)
+                
+                # If there isn't any augmentation, take the scatting length without scaling it [b_value = scattering length * mol fraction]
                 else:
                     # bt
                     in_terminal_methyl_b = in_terminal_methyl_b + (sample_lipid.sample_lipid_name.project_user_lipid_name.tm_scattering * sample_lipid.lipid_mol_fraction)
@@ -426,9 +457,16 @@ def adjust_b_values(data, in_sample_lipids, out_sample_lipids, water, d_water, t
                     in_headgroup_b = in_headgroup_b + (sample_lipid.sample_lipid_name.project_user_lipid_name.hg_scattering * sample_lipid.lipid_mol_fraction)
 
         # Outer
+        # Check each lipid in the sample
         for sample_lipid in out_sample_lipids:
+            
+            # If it's a standard lipid, take values from there
             if sample_lipid.sample_lipid_name.project_lipid_name:
+                
+                # If there is an augmentation (i.e. the user has indicated it's deuterated), calculate the scaled values [b_value = (scattering length + net change) * mol fraction]
                 if sample_lipid in out_augment_dict.keys():
+                    
+                    # For standard augmentations
                     if out_augment_dict[sample_lipid].sample_lipid_augment:
                         # bt
                         out_terminal_methyl_b = out_terminal_methyl_b + ((sample_lipid.sample_lipid_name.project_lipid_name.tm_scattering + out_augment_dict[sample_lipid].sample_lipid_augment.tmg_scattering_net_change) * sample_lipid.lipid_mol_fraction)
@@ -436,6 +474,8 @@ def adjust_b_values(data, in_sample_lipids, out_sample_lipids, water, d_water, t
                         out_chain_b = out_chain_b + ((sample_lipid.sample_lipid_name.project_lipid_name.tg_scattering + out_augment_dict[sample_lipid].sample_lipid_augment.tg_scattering_net_change) * sample_lipid.lipid_mol_fraction)
                         # bh
                         out_headgroup_b = out_headgroup_b + ((sample_lipid.sample_lipid_name.project_lipid_name.hg_scattering + out_augment_dict[sample_lipid].sample_lipid_augment.hg_scattering_net_change) * sample_lipid.lipid_mol_fraction)
+                    
+                    # For custom augmentations
                     else:
                         # bt
                         out_terminal_methyl_b = out_terminal_methyl_b + ((sample_lipid.sample_lipid_name.project_lipid_name.tm_scattering + out_augment_dict[sample_lipid].sample_lipid_custom_augment.tmg_scattering_net_change) * sample_lipid.lipid_mol_fraction)
@@ -443,6 +483,8 @@ def adjust_b_values(data, in_sample_lipids, out_sample_lipids, water, d_water, t
                         out_chain_b = out_chain_b + ((sample_lipid.sample_lipid_name.project_lipid_name.tg_scattering + out_augment_dict[sample_lipid].sample_lipid_custom_augment.tg_scattering_net_change) * sample_lipid.lipid_mol_fraction)
                         # bh
                         out_headgroup_b = out_headgroup_b + ((sample_lipid.sample_lipid_name.project_lipid_name.hg_scattering + out_augment_dict[sample_lipid].sample_lipid_custom_augment.hg_scattering_net_change) * sample_lipid.lipid_mol_fraction)
+                
+                # If there isn't any augmentation, take the scatting length without scaling it [b_value = scattering length * mol fraction]
                 else:
                     # bt
                     out_terminal_methyl_b = out_terminal_methyl_b + (sample_lipid.sample_lipid_name.project_lipid_name.tm_scattering * sample_lipid.lipid_mol_fraction)
@@ -450,7 +492,11 @@ def adjust_b_values(data, in_sample_lipids, out_sample_lipids, water, d_water, t
                     out_chain_b = out_chain_b + (sample_lipid.sample_lipid_name.project_lipid_name.tg_scattering * sample_lipid.lipid_mol_fraction)
                     # bh
                     out_headgroup_b = out_headgroup_b + (sample_lipid.sample_lipid_name.project_lipid_name.hg_scattering * sample_lipid.lipid_mol_fraction)
+            
+            # If it's a custom lipid check for user entered values
             else:
+                
+                # If there is an augmentation (i.e. the user has indicated it's deuterated), calculate the scaled values [b_value = (scattering length + net change) * mol fraction]
                 if  sample_lipid in out_augment_dict.keys():
                     # bt
                     out_terminal_methyl_b = out_terminal_methyl_b + ((sample_lipid.sample_lipid_name.project_user_lipid_name.tm_scattering + out_augment_dict[sample_lipid].sample_lipid_custom_augment.tmg_scattering_net_change) * sample_lipid.lipid_mol_fraction)
@@ -458,6 +504,8 @@ def adjust_b_values(data, in_sample_lipids, out_sample_lipids, water, d_water, t
                     out_chain_b = out_chain_b + ((sample_lipid.sample_lipid_name.project_user_lipid_name.tg_scattering + out_augment_dict[sample_lipid].sample_lipid_custom_augment.tg_scattering_net_change) * sample_lipid.lipid_mol_fraction)
                     # bh
                     out_headgroup_b = out_headgroup_b + ((sample_lipid.sample_lipid_name.project_user_lipid_name.hg_scattering + out_augment_dict[sample_lipid].sample_lipid_custom_augment.hg_scattering_net_change) * sample_lipid.lipid_mol_fraction)
+                
+                # If there isn't any augmentation, take the scatting length without scaling it [b_value = scattering length * mol fraction]
                 else:
                     # bt
                     out_terminal_methyl_b = out_terminal_methyl_b + (sample_lipid.sample_lipid_name.project_user_lipid_name.tm_scattering * sample_lipid.lipid_mol_fraction)
@@ -466,6 +514,7 @@ def adjust_b_values(data, in_sample_lipids, out_sample_lipids, water, d_water, t
                     # bh
                     out_headgroup_b = out_headgroup_b + (sample_lipid.sample_lipid_name.project_user_lipid_name.hg_scattering * sample_lipid.lipid_mol_fraction)
 
+    # Return all the b values in an array to be referenced later on
     b_values = [in_chain_b, in_headgroup_b, in_terminal_methyl_b, out_chain_b, out_headgroup_b, out_terminal_methyl_b, water_b, calculated_water_volume]
     
     return(b_values)
@@ -478,6 +527,7 @@ def asymmetrical_paramitize(parameter, in_sample_lipids, out_sample_lipids, data
     d_water = Molecule.objects.get(compound_name='deuterated_water')
     
     # Parameters
+    # Check each value in the database and prepare a parameter (lmfit) object that includes each of them
     fit_parameters = lsq.Parameters()
     fit_parameters.add_many(
         # Inner
@@ -572,6 +622,7 @@ def asymmetrical_paramitize(parameter, in_sample_lipids, out_sample_lipids, data
     )
 
     # Multiple datasets
+    # Create b value parameters for each dataset using adjuest_b_values() - add these values to the parameter object and label them with the id number for their respective dataset
     try:
         for data in datas:
             # Get values for this data
@@ -638,6 +689,7 @@ def asymmetrical_paramitize(parameter, in_sample_lipids, out_sample_lipids, data
                 )
             )
     # Single dataset
+    # If there is only one dataset, the above loop will break and you will cry - therefore do the same thing but without a loop
     except TypeError:
         # Get values for this data
         b_values = adjust_b_values(datas, in_sample_lipids, out_sample_lipids, water, d_water, temp)
@@ -816,11 +868,13 @@ def asym_additional_parameters(parameter, in_sample_lipids, out_sample_lipids, d
     Vco = fit_parameters['out_chain_volume'].value
     Vto = fit_parameters['out_terminal_methyl_volume'].value
 
+    # If Al for the inner or the outer layer is 0 don't bother calculating
     if Ali == 0 or Alo == 0:
         Dbi = 0
         Dci = 0
         Dbo = 0
         Dco = 0
+    # Calculate normal paramteres with regular old math
     else:
         Dbi = (2 * (Vci + Vhi)) / Ali
         Dci = ((2 * Vci) / Ali) / 2
@@ -828,9 +882,13 @@ def asym_additional_parameters(parameter, in_sample_lipids, out_sample_lipids, d
         Dco = ((2 * Vco) / Alo) / 2
 
     # Find peaks
+    # Take the head probability values for each half and use scipy 'signal' to check for the peak in each
     in_peak_index = sig.argrelextrema(in_head_prob, np.greater)[0]
     out_peak_index = sig.argrelextrema(out_head_prob, np.greater)[0]
+
     # Calculate distance
+    # Take the x value for the peaks and do your regular old distance calculation for each side
+    # inner peak -> 0 && 0 <- outer peak
     in_distance = (np.sqrt( (in_x_values[in_peak_index] - 0)**2 + (in_head_prob[in_peak_index] - in_head_prob[-1])**2 ))
     out_distance = (np.sqrt( (0 - out_x_values[out_peak_index])**2 + (out_head_prob[0] - out_head_prob[out_peak_index])**2 ))
 
